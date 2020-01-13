4800a299fd101560ca12ca0763247eda91d85838
Naga Raj <d.nagaraj3007@gmail.com>
2020-01-13 12:26:42 +0530

    deleting initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-//import './App.css';
 
-
-import Posts from './components/Posts'
-import Postform from './components/Postform'
 
 function App() {
   return (
-    <div className="App">
-        <Postform/>
+
+    <div className="App">   
         <hr/>
-        <Posts/>
     </div>
   );
 }
